5d7c640c614fdb61a4e47446fd75dd1818fbd66b
심정민 <shjj09@naver.com>
2021-02-22 15:44:03 +0900

    [ADD] array 실습

---
+++ 210222/array.js
@@ -0,0 +1,10 @@
+var car1 = "Saab";
+var car2 = "Volvo";
+var car3 = "BMW";
+
+var cars = ["Saab","Volvo", "BMW"];
+
+// ["Saab", "Volvo", "BMW"]
+console.log(cars);
+// "Saab"
+console.log(cars[0]);
