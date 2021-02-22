c33bb7ae5475faf505123138d45e2acfb8dcf313
심정민 <shjj09@naver.com>
2021-02-22 15:43:05 +0900

    [ADD] object 실습

---
+++ 210222/object.js
@@ -0,0 +1,34 @@
+var car1 = {
+    name : "sonata",
+    ph : "500ph",
+    start : function(){
+        console.log("engine is starting");
+    },
+    stop : function(){
+        console.log("engine is stopped");
+    }
+}
+
+var car2 = {
+    name : "bmw",
+    ph : "500ph",
+    start : function(){
+        console.log("engine is starting");
+    },
+    stop : function(){
+        console.log("engine is stopped");
+    }
+}
+
+var car3 = {
+    name : "Volvo",
+    ph : "500ph",
+    start : function(){
+        console.log("engine is starting");
+    },
+    stop : function(){
+        console.log("engine is stopped");
+    }
+}
+
+console.log(car3.name);
