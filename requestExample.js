6d5095327c1a3f601f978d9b6dc9fa02df60cfeb
심정민 <shjj09@naver.com>
2021-02-23 14:32:59 +0900

    [ADD] parsing 실습

--- requestExample.js
+++ requestExample.js
@@ -1,6 +1,10 @@
 const request = require('request');
-request('http://www.google.com', function(err,response, body){
-    console.log('error',err);
-    console.log('statusCode:',response && response.statusCode);
-    console.log('body:', body);
+var parseString = require('xml2js').parseString;
+
+request('http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109', function(err,response, body){
+    var xml = body;
+    parseString(xml, function (err, result) {
+        // 배열에 접근 
+        console.dir(result.rss.channel[0].item[0].description[0].header[0].wf);
+    });
 })
