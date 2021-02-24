1b100fb01ab0b55868a9ee34f65594c9ba763eb1
심정민 <shjj09@naver.com>
2021-02-24 11:15:41 +0900

    [ADD] design 코드

--- expressServer.js
+++ expressServer.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const path = require('path');
 const app = express();
+
+// json 타입에 데이터 전송을 허용한다.
+app.use(express.json());
+// form 타입에 데이터 전송을 허용한다.
+app.use(express.urlencoded({extended:false}));
+// to us static asset 
+app.use(express.static(path.join(__dirname, 'public')));
 
 // views 폴더에 view 파일 존재
 app.set('views',__dirname+'/views');
@@ -24,8 +32,13 @@ app.get('/ejs',function(req,res){
     res.render('ejsTest');
 })
 
+app.get('/designSample',function(req,res){
+  res.render('designSample');
+})
+
 app.post('/userData',function(req,res){
   console.log("사용자의 요청이 발생했습니다.");
+  console.log(req.body);
   res.send(true);
 })
 
