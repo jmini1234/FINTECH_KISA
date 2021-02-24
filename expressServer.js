eb0db319f244b3b414c6745a95abf845587e28b7
심정민 <shjj09@naver.com>
2021-02-24 16:58:05 +0900

    [ADD] auth 및 config

--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const path = require('path');
 const app = express();
+const request = require('request');
+var jwt = require('jsonwebtoken');
+var auth = require('./lib/auth')
+
+require('dotenv').config();
 
 // json 타입에 데이터 전송을 허용한다.
 app.use(express.json());
@@ -28,8 +33,112 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.get('/authTest',auth,function(req,res){
+  res.send("정상적으로 로그인 하셨다면 해당 화면이 보입니다.");
+})
+
 app.get('/signup',function(req,res){
   res.render('signup');
 })
+
+app.get('/login',function(req,res){
+  res.render('login');
+})
+
+app.post('/login',function(req,res){
+  var userEmail = req.body.userEmail;
+  var userPassword = req.body.userPassword;
+  var sql = "SELECT * FROM user WHERE email = ?";
+  connection.query(sql, [userEmail], function(err, result){
+      if(err){
+          console.error(err);
+          res.json(0);
+          throw err;
+      }
+      else {
+          if(result.length == 0){
+              res.json(3)
+          }
+          else {
+              var dbPassword = result[0].password;
+              if(dbPassword == userPassword){
+                  var tokenKey = "f@i#n%tne#ckfhlafkd0102test!@#%"
+                  jwt.sign(
+                    {
+                        userId : result[0].id,
+                        userEmail : result[0].email
+                    },
+                    tokenKey,
+                    {
+                        expiresIn : '10d',
+                        issuer : 'fintech.admin',
+                        subject : 'user.login.info'
+                    },
+                    function(err, token){
+                        console.log('로그인 성공', token)
+                        res.json(token)
+                    }
+                  )            
+              }
+              else {
+                  res.json(2);
+              }
+          }
+      }
+  })
+})
+
+app.post('/signup',function(req,res){
+  // 사용자가 입력한 정보 저장
+  var userName = req.body.userName;
+  var userEmail = req.body.userEmail;
+  var userPassword = req.body.userPassword;
+  var userAccessToken = req.body.userAccessToken;
+  var userRefreshToken = req.body.userRefreshToken;
+  var userSeqNo = req.body.userSeqNo;
+
+  var sql = "INSERT INTO user (name, email, password, accesstoken, refreshtoken, userseqno) VALUES (?,?,?,?,?,?)";
+  connection.query(sql,[userName, userEmail, userPassword, userAccessToken, userRefreshToken, userSeqNo], function(error,results){
+    if(error){
+      console.error(error);
+      throw error;
+    }
+    else{
+      res.json(1);
+    }
+  });
+})
  
+
+// 인증 후 code를 받는 url
+app.get('/authResult',function(req,res){
+  var authCode = req.query.code;
+  var option = {
+      method : "POST",
+      url : "https://testapi.openbanking.or.kr/oauth/2.0/token",
+      header : {
+          'Content-Type' : 'application/x-www-form-urlencoded'
+      },
+      form : {
+        code : authCode,
+        client_id : "4e1719d3-5bc9-47c7-9316-851c72c85038",
+        client_secret : process.env.client_secret,
+        redirect_uri : "http://localhost:3000/authResult",
+        grant_type : "authorization_code"
+      }
+  }
+  request(option, function(err, response, body){
+      if(err){
+          console.error(err);
+          throw err;
+      }
+      else {
+          var accessRequestResult = JSON.parse(body);
+          console.log(accessRequestResult);
+          res.render('resultChild', {data:accessRequestResult});
+      }
+  })
+
+})
+
 app.listen(3000)
