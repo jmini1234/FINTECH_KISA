51e4643ff3c28e11d8fac2d6c83477794e155532
심정민 <shjj09@naver.com>
2021-02-24 10:18:34 +0900

    [ADD] ejs

--- expressServer.js
+++ expressServer.js
@@ -1,5 +1,10 @@
 const express = require('express')
-const app = express()
+const app = express();
+
+// views 폴더에 view 파일 존재
+app.set('views',__dirname+'/views');
+// ejs를 view 엔진으로 사용
+app.set('view engine','ejs');
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
@@ -13,6 +18,10 @@ connection.connect();
  
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+app.get('/ejs',function(req,res){
+    res.render('ejsTest');
 })
 
 app.get('/user', function(req,res){
