63a50d64190e41ca9f76c8825cece24818f455c0
심정민 <shjj09@naver.com>
2021-04-11 22:41:51 +0900

    Move token key to env file

--- expressServer.js
+++ expressServer.js
@@ -22,32 +22,14 @@ app.set('views',__dirname+'/views');
 app.set('view engine','ejs');
 
 var mysql      = require('mysql');
-var db = mysql.createConnection({
+var connection = mysql.createConnection({
   host     : 'us-cdbr-east-03.cleardb.com',
   user     : 'baeb18a87136ed',
   password : '7912102f',
   database : 'heroku_b2e3e1681c45173'
 });
  
-function handleDisconnect() {
-  db.connect(function(err) {            
-    if(err) {                            
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); 
-    }                                   
-  });                                 
-                                         
-  db.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-      return handleDisconnect();                      
-    } else {                                    
-      throw err;                              
-    }
-  });
-}
-
-handleDisconnect();
+connection.connect();
  
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -86,7 +68,7 @@ app.post('/login',function(req,res){
           else {
               var dbPassword = result[0].password;
               if(dbPassword == userPassword){
-                  var tokenKey = "f@i#n%tne#ckfhlafkd0102test!@#%"
+                  var tokenKey = process.env.token_key
                   jwt.sign(
                     {
                         userId : result[0].id,
